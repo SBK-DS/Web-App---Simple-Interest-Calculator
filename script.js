eff86836a806a420b703bf083dd7ff417cd7a3a7
SBK-DS <syedbasitkhurshid@gmail.com>
2021-11-18 21:21:30 -0800

    Range bar problem resolved

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-
-let x = document.getElementById("RangeBar").value;
+function Save()
+{
+    let x = document.getElementById("Rate").value;
+    document.getElementById("VC").innerHTML = x;
+}
 
 
 function compute()
 {
     p = document.getElementById("principal").value;
+    console.log(p);
     
 }
         
